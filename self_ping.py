753b4f9327e5ea7716a4821e8ef82183032df30e
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2025-12-06 23:28:12 +0300

    Create self_ping.py

---
+++ self_ping.py
@@ -0,0 +1,22 @@
+import threading
+import time
+import os
+import requests
+
+def keep_alive():
+    url = os.getenv("SELF_URL")
+    wait = int(os.getenv("SELF_PING_INTERVAL", 240))
+
+    if not url:
+        return
+
+    while True:
+        try:
+            requests.get(url)
+        except:
+            pass
+        time.sleep(wait)
+
+def start_self_ping():
+    t = threading.Thread(target=keep_alive, daemon=True)
+    t.start()
