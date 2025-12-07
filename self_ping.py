5096b5dc32b04f9babfedf6068874c1dba0666e1
muratarslan35 <102037882+muratarslan35@users.noreply.github.com>
2025-12-07 18:41:24 +0300

    Update self_ping.py

--- self_ping.py
+++ self_ping.py
@@ -3,18 +3,16 @@ import time
 import requests
 import os
 
-def keep_alive():
-    url = os.getenv("SELF_URL")
-    wait = int(os.getenv("SELF_PING_INTERVAL", 240))
+def start_self_ping():
+    url = os.getenv('SELF_URL')
+    interval = int(os.getenv('SELF_PING_INTERVAL', 300))
     if not url:
         return
-    while True:
-        try:
-            requests.get(url)
-        except:
-            pass
-        time.sleep(wait)
-
-def start_self_ping():
-    t = threading.Thread(target=keep_alive, daemon=True)
-    t.start()
+    def ping_loop():
+        while True:
+            try:
+                requests.get(url)
+            except:
+                pass
+            time.sleep(interval)
+    threading.Thread(target=ping_loop, daemon=True).start()
